412c83fe4614fb2f9bc9225a7d92b54f3d9e8f3d
JurRodi <rodijurrien@gmail.com>
2022-04-06 10:13:44 +0200

    Added a nav include

---
+++ partials/nav.inc.php
@@ -0,0 +1,9 @@
+<nav>
+    <div>
+        <a href="index.php">IMDribble</a>
+        <a href="profile.php">Profile</a>
+    </div>
+    <div>
+        <a href="logout.php">Logout</a>
+    </div>
+</nav>
