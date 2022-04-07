a3ff4a8ccb9d4d6cb85e37e4d6e4fba9aa79e867
JurRodi <rodijurrien@gmail.com>
2022-04-07 16:16:21 +0200

    Added file for uploading avatar

---
+++ upload.php
@@ -0,0 +1,37 @@
+<?php 
+
+    include_once(__DIR__. "/bootstrap.php");
+    $conn = Db::getConnection();
+    $statusMsg = '';
+    $targetDir = "avatars/";
+
+    if(isset($_POST['submit'])){
+        if(!empty($_FILES["avatar"]["name"])){
+            $fileName = basename($_FILES["avatar"]["name"]);
+            $targetFilePath = $targetDir . $fileName;
+            $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+            $allowTypes = array('jpg','png','jpeg','gif');
+
+            if(in_array($fileType, $allowTypes)){
+                if(move_uploaded_file($_FILES["avatar"]["tmp_name"], $targetFilePath)){
+                    $insert = $conn->prepare("INSERT INTO avatars (fileName, user_id) VALUES (:fileName, :user_id)");
+                    $insert->bindValue(':fileName', $fileName);
+                    $insert->bindValue(':user_id', $user['id']);
+                    $insert->execute();
+                    if($insert){
+                        $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+                    }else{
+                        $statusMsg = "File upload failed, please try again.";
+                    }
+                }else{
+                    $statusMsg = "Sorry, there was an error uploading your file.";
+                }
+            }else{
+                $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
+            }
+        }else{
+            $statusMsg = 'Please select a file to upload.';
+        }
+    }
+
+?>
