2bcb4f51113e5854e254cb31fc21cfd17aa73685
JurRodi <rodijurrien@gmail.com>
2022-04-07 16:56:17 +0200

    Added avatar to profile page

--- profile.php
+++ profile.php
@@ -2,7 +2,13 @@
 
     include_once(__DIR__. "/bootstrap.php");
     Security::onlyLoggedInUsers();
+    $conn = Db::getConnection();
     $user = User::getUserByEmail($_SESSION['email']);
+
+    $statement = $conn->prepare("SELECT * FROM avatars WHERE user_id = :user_id");
+    $statement->bindValue(':user_id', $user['id']);
+    $statement->execute();
+    $avatar = $statement->fetch();
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -16,7 +22,7 @@
     <?php include_once(__DIR__ . "/partials/nav.inc.php"); ?>
     <a href="settings.php">settings</a>
     <div class="avatar">
-        <img src="<?php echo $user['avatar']; ?>" alt="avatar">
+        <img src="<?php echo 'avatars/'.$avatar['fileName']; ?>" alt="avatar">
     </div>
     <h3><?php echo $user['username']; ?></h3>
 </body>
