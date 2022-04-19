3e06457977da1ca7858f7c09ce6af976a875b5cb
JurRodi <rodijurrien@gmail.com>
2022-04-19 15:04:59 +0200

    feed visible without login

--- partials/nav.inc.php
+++ partials/nav.inc.php
@@ -5,6 +5,9 @@
         <a href="profile.php">Profile</a>
     </div>
     <div>
+        <?php if(!isset($_SESSION['email'])): ?>
+            <a href="login.php">Log in</a>
+        <?php endif; ?>
         <a href="logout.php">Logout</a>
     </div>
 </nav>
