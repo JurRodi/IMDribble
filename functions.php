b01ed71a7c536145855c579b4cefcc4ea2f619b7
JurRodi <rodijurrien@gmail.com>
2022-04-22 16:11:16 +0200

    Start of function for time calculation

--- functions.php
+++ functions.php
@@ -1,3 +1,8 @@
 <?php 
 
-    
+    function getTimeDiff($time){
+        $currentTime = new DateTime();
+        $calcTime = new DateTime($time);
+        $timeDiff = $calcTime->diff($currentTime);
+        return $timeDiff->format('%y year %m month %d days %h hours %i minutes');
+    }    
