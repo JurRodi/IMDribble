7466e8042a9cea19948fd7d26a9cfd896abcadba
JurRodi <rodijurrien@gmail.com>
2022-05-05 10:01:37 +0200

    Updated time function

--- functions.php
+++ functions.php
@@ -4,5 +4,16 @@
         $currentTime = new DateTime();
         $calcTime = new DateTime($time);
         $timeDiff = $calcTime->diff($currentTime);
-        return $timeDiff->format('%y year %m month %d days %h hours %i minutes');
+        if($timeDiff->y > 0){
+            $displayedTime = $timeDiff->y . " years ago";
+        }elseif($timeDiff->m > 0){
+            $displayedTime = $timeDiff->m . " months ago";
+        }elseif($timeDiff->d > 0){
+            $displayedTime = $timeDiff->d . " days ago";
+        }elseif($timeDiff->h > 0){
+            $displayedTime = $timeDiff->h . " hours ago";
+        }elseif($timeDiff->i > 0){
+            $displayedTime = $timeDiff->i . " minutes ago";
+        }
+        return $displayedTime;
     }    
