bf82fcb7ac3d6ab2ccf9d8477f7fbcc7e4d479cd
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-05-26 16:54:52 +0200

    nog niet merge

--- partials/nav.inc.php
+++ partials/nav.inc.php
@@ -6,11 +6,7 @@
             <i class="material-icons nav-icon">home</i>
             <span class="nav-text">Home</span>
         </a>
-        <a href="#" class="nav-link link-active">
-            <i class="material-icons nav-icon">travel_explore</i>
-            <span class="nav-text">Explore</span>
-        </a>
-        <a href="archive.php" class="nav-link link-active">
+        <a href="addproject.php" class="nav-link link-active">
             <i class="material-icons nav-icon">add_box</i>
             <span class="nav-text">Add project</span>
         </a>
@@ -24,13 +20,12 @@
     <a href="index.php"><img class="logo" src="images/logo.png" alt="Logo"> </a>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="#">Explore?</a></li>
+      
         <li><a href="addProject.php">Add project</a></li>
-        <li><a href="#">saved</a></li>
+       
         <li><a href="profile.php">Profile</a></li>
     </ul>
-
-
+   
     <div>
         <?php if(!isset($_SESSION['email'])): ?>
             <a href="login.php">Log in</a>
@@ -41,4 +36,5 @@
         <?php endif; ?>
         
     </div>
+    
 </nav>
